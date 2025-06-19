9097376d48d93e553d35d2aae73010fd38c3bf83
N1c0l4sM0lf1n <n.molfinqueo01@ufromail.cl>
2025-06-19 10:21:52 -0400

    Mi Version del Codigo

---
+++ src/main/java/Launcher/Main.java
@@ -0,0 +1,10 @@
+package Launcher;
+
+import vista.ConsolaDados;
+
+public class Main {
+    public static void main(String[] args) {
+        ConsolaDados consola = new ConsolaDados();
+        consola.menu();
+    }
+}
